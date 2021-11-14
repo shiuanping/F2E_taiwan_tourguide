93497b8fec4d6155975e1adad97c707b07a601dc
shiuanping <shiuanping105@gmail.com>
2021-11-15 00:53:16 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // devServer: {
-  //   proxy: 'http://localhost:8080'
-  // },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/F2E_taiwan_tourguidegit/'
     : '/'
